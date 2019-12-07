ebb427bb9205c786b4b169342e8e55c3984048fc
tmaulon <thomas.maulon@gmail.com>
2019-12-07 17:40:58 +0100

    add admin aliment modification page

--- src/Controller/Admin/AdminAlimentController.php
+++ src/Controller/Admin/AdminAlimentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Aliment;
 use App\Repository\AlimentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,6 +18,17 @@ class AdminAlimentController extends AbstractController
         return $this->render('admin/admin_aliment/adminAliment.html.twig', [
             'controller_name' => 'AdminAlimentController',
             'aliments' => $aliments
+        ]);
+    }
+
+    /**
+     * @Route("/admin/aliment/{id}", name="admin_aliment_modification")
+     */
+    public function modification(Aliment $aliment)
+    {
+        return $this->render('admin/admin_aliment/modificationAliment.html.twig', [
+            'controller_name' => 'AdminAlimentController',
+            'aliment' => $aliment
         ]);
     }
 }
